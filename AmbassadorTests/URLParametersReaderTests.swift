edf029d77693afc8e8e79c9863739fe8ce5fe5f2
Victor Lin <bornstub@gmail.com>
2016-06-10 15:26:55 -0700

    Add test for URL parameter parsing

---
+++ AmbassadorTests/URLParametersReaderTests.swift
@@ -0,0 +1,46 @@
+//
+//  URLParametersReaderTests.swift
+//  Ambassador
+//
+//  Created by Fang-Pen Lin on 6/10/16.
+//  Copyright © 2016 Fang-Pen Lin. All rights reserved.
+//
+
+import XCTest
+
+import Ambassador
+
+class URLParametersReaderTests: XCTestCase {
+    func testParseURLParameter() {
+        let params1 = URLParametersReader.parseURLParameters("foo=bar&eggs=spam")
+        XCTAssertEqual(params1.count, 2)
+        XCTAssertEqual(params1.first?.0, "foo")
+        XCTAssertEqual(params1.first?.1, "bar")
+        XCTAssertEqual(params1.last?.0, "eggs")
+        XCTAssertEqual(params1.last?.1, "spam")
+
+        let params2 = URLParametersReader.parseURLParameters("foo%5Bbar%5D=eggs%20spam")
+        XCTAssertEqual(params2.count, 1)
+        XCTAssertEqual(params2.first?.0, "foo[bar]")
+        XCTAssertEqual(params2.first?.1, "eggs spam")
+    }
+
+    func testURLParameterReader() {
+        let input = { (handler: ([UInt8] -> Void)?) in
+            handler!(Array("foo ".utf8))
+            handler!(Array("=".utf8))
+            handler!(Array("bar".utf8))
+            handler!(Array("&eggs=spam".utf8))
+            handler!([])
+        }
+        var receivedParams: [(String, String)]!
+        URLParametersReader.read(input) { params in
+            receivedParams = params
+        }
+        XCTAssertEqual(receivedParams.count, 2)
+        XCTAssertEqual(receivedParams.first?.0, "foo")
+        XCTAssertEqual(receivedParams.first?.1, "bar")
+        XCTAssertEqual(receivedParams.last?.0, "eggs")
+        XCTAssertEqual(receivedParams.last?.1, "spam")
+    }
+}
