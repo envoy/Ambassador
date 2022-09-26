d0ca7e2af0e911192fb4ce1f406e11897d34aac2
Andreas Grosam <andreas.grosam@meona.de>
2022-09-26 12:59:02 +0200

    User Debendency Fork Embassy

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Ambassador", targets: ["Ambassador"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/envoy/Embassy.git", from: "4.0.5")
+        .package(url: "https://github.com/couchdeveloper/Embassy.git", from: "4.0.6")
     ],
     targets: [
         .target(name: "Ambassador", dependencies: ["Embassy"], path: "Ambassador"),
