7d5f951c01f41adbfdf387936019cc08e56c30e0
Matthew Shehan <mattshehan@envoy.com>
2026-08-07 12:58:45 -0700

    update package.swift and swift tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,6 +12,17 @@ let package = Package(
         .package(url: "https://github.com/envoy/Embassy.git", from: "4.0.5")
     ],
     targets: [
-        .target(name: "Ambassador", dependencies: ["Embassy"], path: "Ambassador"),
+        .target(
+            name: "Ambassador",
+            dependencies: ["Embassy"],
+            path: "Ambassador",
+            exclude: ["Ambassador.h", "Info.plist"]
+        ),
+        .testTarget(
+            name: "AmbassadorTests",
+            dependencies: ["Ambassador", "Embassy"],
+            path: "AmbassadorTests",
+            exclude: ["Info.plist"]
+        ),
     ]
 )
